4485a0bb167f023fc76ba3bd69c7ed5d9613fdb3
Nov-R <116569539@qq.com>
2026-03-24 13:01:32 +0800

    stage3

--- src/components/AppNav.vue
+++ src/components/AppNav.vue
@@ -1,26 +1,21 @@
 <template>
-    <div>
-        <nav>
-            <router-link to="/home">首页</router-link> |
-            <router-link to="/dashboard">仪表盘</router-link> |
-            <router-link to="/settings">设置</router-link> |
-            <button @click="handleLogout">退出登录</button>
-        </nav>
-    </div>
+    <nav>
+        <router-link to="/home">首页</router-link> |
+        <router-link to="/dashboard">仪表盘</router-link> |
+        <router-link to="/settings">设置</router-link> 
+        <button @click="handleLogout">退出登录</button>
+    </nav>
 </template>
 
 <script setup lang="ts">
-    import { useRouter } from 'vue-router'
-    import { useAuthStore } from '@/stores/auth'
+import { useRouter } from 'vue-router'
+import { useAuthStore } from '@/stores/auth'
 
-    const router = useRouter()
-    const auth = useAuthStore()
+const router = useRouter()
+const auth = useAuthStore()
 
-    function handleLogout() {
+function handleLogout() {
     auth.logout()
     router.push('/login')
-    }
+}
 </script>
-
-<style scoped>
-</style>
